51c6a71cf3b8d83b9b10b4b0cf81715c3838b472
Christopher Guy <softcorpindustries@gmail.com>
2021-11-18 15:06:57 -0700

    clean start

---
+++ dist/js/nav.js
@@ -0,0 +1,26 @@
+const menuBtn = document.querySelector(".navBtn__menu");
+const pageNavList = document.getElementById('pageNav-list')
+const cHeader = document.querySelector(".cHeader");
+const navBarSite = document.getElementById('siteNav');
+const darkMode = document.getElementById('darkMode');
+
+let menuOpen = false;
+
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    pageNavList.classList.add("openNav");
+    menuBtn.classList.add("openNav");
+    cHeader.classList.add("openNav");
+    navBarSite.classList.add('showNav');
+    darkMode.classList.add('showDarkMode');
+    menuOpen = true;
+  } else {
+    pageNavList.classList.remove("openNav");
+    menuBtn.classList.remove("openNav");
+    cHeader.classList.remove("openNav");
+    navBarSite.classList.remove('showNav');
+    darkMode.classList.remove('showDarkMode');
+    menuOpen = false;
+  }
+
+})
