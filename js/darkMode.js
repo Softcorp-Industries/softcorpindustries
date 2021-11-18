20bec5115e2e3c4614934b3a33cbaa206fbbf13f
Christopher Guy <softcorpindustries@gmail.com>
2021-11-18 15:20:42 -0700

    Updates

---
+++ js/darkMode.js
@@ -0,0 +1,48 @@
+// Dark Mode and Local Storage
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+let darkModeSt = localStorage.getItem("darkModeSt");
+
+let darkModeOn = false;
+
+const enableDarkMode = () => {
+  document.body.setAttribute("data-darkMode", "darkMode");
+  darkModeBtn.style.backgroundImage = "url(img/icon/darkMode-icon-light512.webp)"
+  darkModeBtn.classList.add("darkModeAnim");
+  darkModeBtn.classList.remove("darkModeAnimRev");
+  localStorage.setItem("darkModeSt", "enabled");
+};
+const disableDarkMode = () => {
+  document.body.setAttribute("data-darkMode", "light");
+  darkModeBtn.style.backgroundImage = "url(img/icon/darkMode-icon.png)"
+  darkModeBtn.classList.add("darkModeAnimRev");
+  darkModeBtn.classList.remove("darkModeAnim");
+  localStorage.setItem("darkModeSt", "disabled");
+};
+if (darkModeSt === "enabled") {
+  enableDarkMode();
+}
+function darkModeActive() {
+  document.addEventListener("DOMContentLoaded", () => {
+    darkModeBtn.addEventListener("click", () => {
+      darkModeSt = localStorage.getItem("darkModeSt");
+      if (darkModeSt !== "enabled" && !darkModeOn) {
+        enableDarkMode();
+        trans();
+        darkModeOn = true;
+      } else {
+        disableDarkMode();
+        trans();
+        darkModeOn = false;
+      }
+    });
+  });
+}
+darkModeActive();
+
+let trans = () => {
+  document.body.classList.add("transition");
+  window.setTimeout(() => {
+    document.body.classList.remove("transition");
+  }, 1000);
+};
---
+++ js/greeting.js
@@ -0,0 +1,17 @@
+const date = new Date();
+const hours = date.getHours();
+
+let greeting;
+
+if (hours < 12) {
+    greeting = "Good Morning";
+} else if
+(hours >= 12 && hours < 17) {
+    greeting = "Good Afternoon";
+}
+else {
+    greeting = "Good Evening"
+}
+document.getElementById('greeting').innerHTML = greeting;
+
+
