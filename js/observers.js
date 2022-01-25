aef3a1b815c8339522c65274781b02b8a162d6c5
Christopher Guy <softcorpindustries@gmail.com>
2022-01-25 14:24:38 -0700

    Updates

--- js/observers.js
+++ js/observers.js
@@ -3,22 +3,24 @@ const txtSliders = document.querySelectorAll('.txtSlider');
 
 /*  This sets the Options for the Observer  */
 const slideOptions = {
-    rootMargin: "0px 0px -100px 0px",
-    threshold: 1
+    rootMargin: "-150px",
+    threshold: 0
 }
 
-/*  This Creates the IntersectionObserver()   */
+/*  This Creates the IntersectionObserver() called "slideOnScroll"   */
 const slideOnScroll = new IntersectionObserver(function(entries, slideOnScroll) {
     entries.forEach(entry => {
-        if(!isIntersecting) {
+        if(!entry.isIntersecting) {
             return;
         } else {
             entry.target.classList.add("slideTxt");
             slideOnScroll.unobserve(entry.target);
         }
-    })
+    });
 }, slideOptions);
 
+
+/*  This Calls the IntersectionObserver  */
 txtSliders.forEach(txtslider => {
     slideOnScroll.observe(txtslider);
 });
