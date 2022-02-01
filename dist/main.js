18b9f74460366f141511dc9c3c10cfaf5fad87b5
Christopher Guy <softcorpindustries@gmail.com>
2022-02-01 13:38:52 -0700

    lazy load and lazy background

--- dist/main.js
+++ dist/main.js
@@ -3,3 +3,5 @@ import * as DarkMode from './js/darkMode.js';
 import * as Greeting from './js/greeting.js';
 import * as ShowBtns from './js/showBtns.js';
 import * as Observers from './js/observers.js';
+import * as Lazysizes from './js/lazysizes.min.js';
+import * as Bgset from './js/ls.bgset.min.js';
