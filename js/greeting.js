34f6ae0403741b67c0bfa476f13e0a462c68c751
Christopher Guy <softcorpindustries@gmail.com>
2022-02-22 16:19:04 -0700

    Updates

--- js/greeting.js
+++ js/greeting.js
@@ -15,3 +15,20 @@ else {
 document.getElementById('greeting').innerHTML = greeting;
 
 
+let certModal1 = document.getElementById("certModal1");
+
+let certImg1 = document.getElementById("certImg1");
+let modalImg1 = document.getElementById("modalImg1");
+let captionText = document.getElementById("caption");
+
+certImg1.onclick = function() {
+    certModal1.style.display = "block";
+    modalImg1.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    certModal1.style.display = "none";
+}
