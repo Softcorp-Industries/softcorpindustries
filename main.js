8304defeb237ff5b7a0ecd9757fcb76fc8137ea2
Christopher Guy <softcorpindustries@gmail.com>
2022-03-20 10:08:44 -0600

    Updates

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import * as NavMenu from './js/nav.js';
 import * as DarkMode from './js/darkMode.js';
+import * as Lazysizes from './js/lazysizes.min.js';
+import * as Bgset from './js/ls.bgset.min.js';
 import * as Greeting from './js/greeting.js';
 import * as ShowBtns from './js/showBtns.js';
 import * as Observers from './js/observers.js';
-import * as Lazysizes from './js/lazysizes.min.js';
-import * as Bgset from './js/ls.bgset.min.js';
 import * as Test from './js/test.js';
