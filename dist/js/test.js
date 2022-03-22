0e67ff0168c71978944d77a2d493560cb1dde8eb
Christopher Guy <softcorpindustries@gmail.com>
2022-03-22 11:31:24 -0600

    tablet controls

--- dist/js/test.js
+++ dist/js/test.js
@@ -1,18 +1,51 @@
 //alert('fuck you');
-let certModal1 = document.getElementById("certModal1");
+const slides = document.querySelectorAll(".constructionSlide");
+const next = document.querySelector("#nextBtn");
+const prev = document.querySelector("#prevBtn");
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
 
-let certImg1 = document.getElementById("certImg1");
-let modalImg1 = document.getElementById("modalImg1");
-let captionText = document.getElementById("caption");
-
-certImg1.onclick = function () {
-  certModal1.style.display = "block";
-  modalImg1.dataset.src = this.dataset.src;
-  captionText.innerHTML = this.alt;
+const nextSlide = () => {
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add("current");
+  } else {
+    slides[0].classList.add("current");
+  }
+  //setTimeout(() => current.classList.remove("current"));
+};
+const prevSlide = () => {
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  if (current.previousElementSibling) {
+    current.previousElementSibling.classList.add("current");
+  } else {
+    slides[slides.length - 1].classList.add("current");
+  }
+  //setTimeout(() => current.classList.remove("current"));
+};
+next.addEventListener("click", () => {
+  nextSlide();
+/*
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+*/
+});
+prev.addEventListener("click", () => {
+  prevSlide();
+/*
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+  */
+});
+/*
+if (auto) {
+  slideInterval = setInterval(nextSlide, intervalTime);
 }
-
-let span = document.getElementsByClassName("close")[0];
-
-span.onclick = function () {
-  certModal1.style.display = "none";
-}
+*/
