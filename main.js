a2733ae4f750cea9b5c72b9492ed69bf27e8c9d6
Christopher Guy <softcorpindustries@gmail.com>
2022-08-30 13:04:52 -0600

    Updates

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ import * as Test from './js/test.js';
 import * as Lazysizes from './js/lazysizes.min.js';
 import * as Bgset from './js/ls.bgset.min.js';
 import * as Observers from './js/observers.js';
+import * as Slider from './js/slider.js';
