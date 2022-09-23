7850352adfc92219a76b45a5a23628eeddee8dfa
Christopher Guy <softcorpindustries@gmail.com>
2022-09-22 19:14:16 -0600

    Updates

--- homeWork.js
+++ homeWork.js
@@ -1,1 +1,6 @@
-import * as homeWorkShowBtns  from './js/HomeWork/homeWorkShowBtns.js';
+import * as homeWorkShowBtns from './js/HomeWork/homeWorkShowBtns.js';
+
+import * as BtnProjects from './js/HomeWork/BtnProjects/btnProjects.js';
+import * as ImgProjects from './js/HomeWork/ImgProjects/imgProjects.js';
+import * as StorageProjects from './js/HomeWork/StorageProjects/storageProjects.js';
+import * as WebPlayerProjects from './js/HomeWork/WebPlayerProjects/webPlayerProjects.js';
