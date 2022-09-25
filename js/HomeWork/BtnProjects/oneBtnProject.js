165ecccc35bfb349182e0f5bf6ac8f82a2cb770b
Christopher Guy <softcorpindustries@gmail.com>
2022-09-25 16:26:13 -0600

    Updates

--- js/HomeWork/BtnProjects/oneBtnProject.js
+++ js/HomeWork/BtnProjects/oneBtnProject.js
@@ -1,19 +1,21 @@
 
-
 let selectedBtn = false;
 
 // Grabs the Button that was "clicked" within the Element that has the "ID" of btn
 
 document.querySelector("#btn").addEventListener("click", (e) => {
-  console.log(e.target);
+  //console.log(e.target);
   
   // Add an active Class to the Button Clicked
 
-  if (!selectedBtn && e.target.classList.contains("btn")) {
+  if (!selectedBtn && e.target.classList.contains("btnProject-btn")) {
     e.target.classList.add("active");
+    console.log(e.target.parentElement.parentElement);
+    e.target.parentElement.previousElementSibling.style.display = "block";
     selectedBtn = true;
   } else {
     e.target.classList.remove("active");
+    e.target.parentElement.previousElementSibling.style.display = "none";
     selectedBtn = false;
   }
 });
