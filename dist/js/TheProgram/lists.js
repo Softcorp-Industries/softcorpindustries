5fd5cb7987b26aa7c13864cd3d79a179ba19d363
Christopher Guy <softcorpindustries@gmail.com>
2022-10-13 16:15:12 -0600

    the program

---
+++ dist/js/TheProgram/lists.js
@@ -0,0 +1,21 @@
+import { Display } from './display.js';
+
+export class Lists {
+  static getFullTechList() {
+    const fullEmployeeList = JSON.parse(localStorage.getItem("employeeStorage"));
+    const techs = fullEmployeeList.filter(tech => tech.position === "Tech");
+    //console.log(techs);
+    techs.forEach((tech) => Display.buildFullTechTable(tech))
+  }
+  static getFullOfficeList() {
+    const fullEmployeeList = JSON.parse(localStorage.getItem("employeeStorage"));
+    const officeStaff = fullEmployeeList.filter(staff => staff.position === "Office");
+    officeStaff.forEach((staff) => Display.buildOfficeStaffTable(staff))
+    //console.log(officeStaff)
+  }
+  static getFullManagementList() {
+
+  }
+}
+
+
