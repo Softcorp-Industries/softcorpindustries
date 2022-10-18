640fa09b1a20f78be9cb4103ce052497e84e9b8d
Christopher Guy <softcorpindustries@gmail.com>
2022-10-17 18:48:40 -0600

    responsive dashboard

--- dist/js/TheProgram/lists.js
+++ dist/js/TheProgram/lists.js
@@ -5,16 +5,18 @@ export class Lists {
     const fullEmployeeList = JSON.parse(localStorage.getItem("employeeStorage"));
     const techs = fullEmployeeList.filter(tech => tech.position === "Tech");
     //console.log(techs);
-    techs.forEach((tech) => Display.buildFullTechTable(tech))
+    techs.forEach((tech) => Display.buildFullTechTable(tech));
   }
   static getFullOfficeList() {
     const fullEmployeeList = JSON.parse(localStorage.getItem("employeeStorage"));
     const officeStaff = fullEmployeeList.filter(staff => staff.position === "Office");
-    officeStaff.forEach((staff) => Display.buildOfficeStaffTable(staff))
+    officeStaff.forEach((staff) => Display.buildOfficeStaffTable(staff));
     //console.log(officeStaff)
   }
   static getFullManagementList() {
-
+    const fullEmployeeList = JSON.parse(localStorage.getItem("employeeStorage"));
+    const management = fullEmployeeList.filter(manager => manager.position === "Manager");
+    management.forEach((manager) => Display.buildFullManagementTable(manager));
   }
 }
 
